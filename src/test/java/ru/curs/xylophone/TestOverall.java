0e37dcc6fb954d2b5ef808052fe4ce02976d611d
vladimir.kurov <vladimir.kurov@simbirsoft.com>
2018-09-20 17:13:12 +0400

    kel-77 - added test for conversion excel file with rouble and percent symbols into pdf - added test for xylophone process with test-data xml including rouble and percent symbols

--- src/test/java/ru/curs/xylophone/TestOverall.java
+++ src/test/java/ru/curs/xylophone/TestOverall.java
@@ -1,13 +1,23 @@
 package ru.curs.xylophone;
 
-import static org.junit.Assert.assertTrue;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Paths;
 
-import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class TestOverall {
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
 	@Test
 	public void test1() throws XML2SpreadSheetError {
 		InputStream descrStream = TestReader.class
@@ -54,4 +64,31 @@ public class TestOverall {
 		 * finally { out.close(); }
 		 */
 	}
+
+	@Test
+	public void checkGenerateResultXlsFileWithSpecialSymbolsInDataXmlShouldSuccess() throws Exception {
+	    File descriptor = Paths.get(TestOverall.class.getResource("testdescriptor.xml").toURI()).toFile();
+	    InputStream dataStream = TestReader.class
+				.getResourceAsStream("test_data_with_spec_symbols.xml");
+	    File template = Paths.get(TestOverall.class.getResource("template.xls").toURI()).toFile();
+
+        File createdTempOutputFile = temporaryFolder.newFile("temp.xls");
+
+        try (OutputStream outputStream = new FileOutputStream(createdTempOutputFile)) {
+            XML2Spreadsheet.process(dataStream, descriptor, template,
+                    false, false, outputStream);
+        }
+
+        NPOIFSFileSystem fileSystem = new NPOIFSFileSystem(createdTempOutputFile);
+        HSSFWorkbook workbook = new HSSFWorkbook(fileSystem.getRoot(), false);
+
+        Excel2Print excelPrinter = new Excel2Print(workbook);
+        excelPrinter.setFopConfig(Paths.get(TestFO.class.getResource("fop.xconf").toURI()).toFile());
+
+        File pdfResultFile = temporaryFolder.newFile("after_conversion_to_pdf.pdf");
+        excelPrinter.toPDF(new FileOutputStream(pdfResultFile));
+
+        assertTrue(createdTempOutputFile.length() > 0);
+        assertTrue(pdfResultFile.length() > 0);
+    }
 }
